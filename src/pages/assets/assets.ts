ce4cb310235b19fdfe4c3a1b49a9b2832a08bb7a
pxc0k2p <pxc0k2p@nee.com>
2017-05-08 10:35:46 -0400

    HardCoding miso user nodes

--- src/pages/assets/assets.ts
+++ src/pages/assets/assets.ts
@@ -42,16 +42,44 @@ export class AssetsPage {
     else {
       this.showSearch = true;
       if (this.firstsearchtext != "") {
-        this.showNoNodesFound = false;
-        this.showLoading();
+
         if (this.authService.currentUser.role == "Admin") {
+          this.showNoNodesFound = false;
+          this.showLoading();
           this.assetsAPI(null);
         }
+      }
         else if (this.authService.currentUser.role == "Misoadmin") {
-          this.assetsAPI("miso");
+          this.assets =
+            [
+              { "NY_NodeName": "OTP.ASHTA_FPLP", "NY_NodeID": "OTP.ASHTA_FPLP" },
+              { "NY_NodeName": "OTP.ASHTAII", "NY_NodeID": "OTP.ASHTAII" },
+              { "NY_NodeName": "OTP.ASHTAIII", "NY_NodeID": "OTP.ASHTAIII" },
+              { "NY_NodeName": "ALTW.CRYSTAL1", "NY_NodeID": "ALTW.CRYSTAL1" },
+              { "NY_NodeName": "ALTW.CRYSTAL2", "NY_NodeID": "ALTW.CRYSTAL2" },
+              { "NY_NodeName": "ALTW.CRYSTAL3", "NY_NodeID": "ALTW.CRYSTAL3" },
+              { "NY_NodeName": "ALTW.ENDV", "NY_NodeID": "ALTW.ENDV" },
+              { "NY_NodeName": "ALTW.ENDV1", "NY_NodeID": "ALTW.ENDV1" },
+              { "NY_NodeName": "ALTW.ENDV2", "NY_NodeID": "ALTW.ENDV2" },
+              { "NY_NodeName": "ALTW.ENDV3", "NY_NodeID": "ALTW.ENDV3" },
+              { "NY_NodeName": "ALTW.ENDV4", "NY_NodeID": "ALTW.ENDV4" },
+              { "NY_NodeName": "ALTW.ENDVI", "NY_NodeID": "ALTW.ENDVI" },
+              { "NY_NodeName": "OTP.LANGDN1", "NY_NodeID": "OTP.LANGDN1" },
+              { "NY_NodeName": "OTP.LANGDN2", "NY_NodeID": "OTP.LANGDN2" },
+              { "NY_NodeName": "ALTW.MOWERCO", "NY_NodeID": "ALTW.MOWERCO" },
+              { "NY_NodeName": "ALTW.MOWERCO1", "NY_NodeID": "ALTW.MOWERCO1" },
+              { "NY_NodeName": "MP.OLIVER12", "NY_NodeID": "MP.OLIVER12" },
+              { "NY_NodeName": "MP.OLIVERC2", "NY_NodeID": "MP.OLIVERC2" },
+              { "NY_NodeName": "MP.OLIVERCO", "NY_NodeID": "MP.OLIVERCO" },
+              { "NY_NodeName": "ALTW.STORYCO", "NY_NodeID": "ALTW.STORYCO" },
+              { "NY_NodeName": "ALTW.STORYCOII", "NY_NodeID": "ALTW.STORYCOII" },
+              { "NY_NodeName": "CONS.TUSCOLA1", "NY_NodeID": "CONS.TUSCOLA1" },
+              { "NY_NodeName": "DECO.TUSCOLA2", "NY_NodeID": "DECO.TUSCOLA2" }
+
+            ]
         }
 
-      }
+      
     }
 
   }
